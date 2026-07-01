b2858e34caffe9df9aaf9272e3eaf6bf83296ea6
sumedh-NX <sumedh.nx@gmail.com>
2026-07-01 18:26:25 +0530

    Vite config for Live

--- vite.config.js
+++ vite.config.js
@@ -21,8 +21,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',               // <--- CHANGE THIS TO ROOT for TEST AND LOCAL,
+  //base: '/',               // <--- CHANGE THIS TO ROOT for TEST AND LOCAL,
   //base: process.env.NODE_ENV === 'production' ? '/wms-frontend/' : '/',
+  base: '/wms-frontend/'
   build: {
     chunkSizeWarningLimit: 1000,
   },
