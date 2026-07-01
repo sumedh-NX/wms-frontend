edf57f385b3c68a8673b4c1664462f5523ec48bd
sumedh-NX <sumedh.nx@gmail.com>
2026-07-01 18:42:53 +0530

    Test vita congif

--- vite.config.js
+++ vite.config.js
@@ -22,7 +22,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',               // <--- CHANGE THIS TO ROOT for TEST AND LOCAL,
-  
   //base: '/wms-frontend/',  // <--- CHANGE THIS TO SUBFOLDER for PRODUCTION (GitHub Pages)
   build: {
     chunkSizeWarningLimit: 1000,
